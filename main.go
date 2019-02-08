8ec14c00943af9bdc66b771316439add6fff8a3a
cmok4290 <cmok4290@gmail.com>
2019-02-08 17:09:10 -0500

    add date struct, more handlers and routes

--- main.go
+++ main.go
@@ -1,19 +1,48 @@
 package main
 
 import (
-	"encoding/json"
+	// "encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-	"time"
+	// "time"
 	"github.com/gorilla/mux"
+
 )
 
-func GetTimestamp(w http.ResponseWriter, r *http.Request) {
-	t := time.Now()
+type Datestring struct {
+	date string `json:"datestring"`
+	unix int `json:"unix"`
+	utc string `json:"utc"`
+}
+
+type Dates []Datestring
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Server is running...\n"))
+}
+
+func APIIndex(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Add API documentation...\n"))
+}
+
+func TimestampIndex(w http.ResponseWriter, r *http.Request) {
+	return
+}
+
+func TimestampShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	ds := vars["datestring"]
+	fmt.Fprintln(w, "Timestamp shows:", ds)
 }
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/timestamp", GetTimestamp).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	router.HandleFunc("/", Index).Methods("GET")
+	router.HandleFunc("/api", APIIndex).Methods("GET")
+	router.HandleFunc("/api/", APIIndex).Methods("GET")
+	router.HandleFunc("/api/timestamp", TimestampIndex).Methods("GET")
+	router.HandleFunc("/api/timestamp/", TimestampShow).Methods("GET")
+	router.HandleFunc("/api/timestamp/{datestring}", TimestampShow).Methods("GET")
+	log.Fatal(http.ListenAndServe("localhost:8000", router))
 }
