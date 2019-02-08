9a5430867d213099ab0f923147c72f4032852ed4
cmok4290 <cmok4290@gmail.com>
2019-02-08 18:01:31 -0500

    add content type, status, and return json

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	// "encoding/json"
-	"fmt"
+	"encoding/json"
+	// "fmt"
 	"log"
 	"net/http"
 	// "time"
@@ -11,38 +11,52 @@ import (
 )
 
 type Datestring struct {
-	date string `json:"datestring"`
-	unix int `json:"unix"`
-	utc string `json:"utc"`
+	Date string `json:"date"`
+	Unix int `json:"unix"`
+	UTC string `json:"utc"`
 }
 
-type Dates []Datestring
+// type Dates []Datestring
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Server is running...\n"))
+	w.WriteHeader(http.StatusOK)
 }
 
 func APIIndex(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Add API documentation...\n"))
+	w.WriteHeader(http.StatusOK)
 }
 
 func TimestampIndex(w http.ResponseWriter, r *http.Request) {
-	return
+	w.Write([]byte("Another index..."))
+	w.WriteHeader(http.StatusOK)
 }
 
 func TimestampShow(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	ds := vars["datestring"]
-	fmt.Fprintln(w, "Timestamp shows:", ds)
+
+	date := Datestring{Date: ds, Unix: 5, UTC: "UTC"}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(date); err != nil {
+		panic(err)
+	}
+
+	// fmt.Fprintln(w, "Timestamp shows:", ds)
 }
 
 func main() {
 	router := mux.NewRouter()
+
 	router.HandleFunc("/", Index).Methods("GET")
 	router.HandleFunc("/api", APIIndex).Methods("GET")
 	router.HandleFunc("/api/", APIIndex).Methods("GET")
 	router.HandleFunc("/api/timestamp", TimestampIndex).Methods("GET")
 	router.HandleFunc("/api/timestamp/", TimestampShow).Methods("GET")
 	router.HandleFunc("/api/timestamp/{datestring}", TimestampShow).Methods("GET")
+	
 	log.Fatal(http.ListenAndServe("localhost:8000", router))
 }
