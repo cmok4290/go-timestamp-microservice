42cce66c335dd292523b647674b03c40f557efef
cmok4290 <cmok4290@gmail.com>
2019-02-12 17:09:23 -0500

    refactor routes

--- main.go
+++ main.go
@@ -20,45 +20,6 @@ type Datestring struct {
 
 // type Dates []Datestring
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Server is running...\n"))
-	w.WriteHeader(http.StatusOK)
-}
-
-func APIIndex(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Add API documentation...\n"))
-	w.WriteHeader(http.StatusOK)
-}
-
-func TimestampIndex(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Another index..."))
-	w.WriteHeader(http.StatusOK)
-}
-
-func TimestampShow(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	ds := vars["datestring"]
-	ms := uint64(0)
-
-	re := regexp.MustCompile("[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}")
-	if re.MatchString(ds) != true {
-		// if s, err := strconv.ParseUint(ds, 10, 64); err == nil {
-		// 	ms = s
-		// }
-		ms = s
-	}
-
-	date := Datestring{Date: ds, Unix: 5, UTC: "UTC"}
-
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(date); err != nil {
-		panic(err)
-	}
-
-	// fmt.Fprintln(w, "Timestamp shows:", ds)
-}
-
 func main() {
 	router := mux.NewRouter()
 
