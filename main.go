3bfc0a85a14b0ab168fa04a64b033efa94ced33a
cmok4290 <cmok4290@gmail.com>
2019-02-12 17:13:46 -0500

    refactor main

--- main.go
+++ main.go
@@ -1,34 +1,13 @@
 package main
 
 import (
-	"encoding/json"
-	// "fmt"
 	"log"
 	"net/http"
-	"regexp"
-	"strconv"
-	// "time"
-	"github.com/gorilla/mux"
-
 )
 
-type Datestring struct {
-	Date string `json:"date"`
-	Unix int `json:"unix"`
-	UTC string `json:"utc"`
-}
-
-// type Dates []Datestring
-
 func main() {
-	router := mux.NewRouter()
 
-	router.HandleFunc("/", Index).Methods("GET")
-	router.HandleFunc("/api", APIIndex).Methods("GET")
-	router.HandleFunc("/api/", APIIndex).Methods("GET")
-	router.HandleFunc("/api/timestamp", TimestampIndex).Methods("GET")
-	router.HandleFunc("/api/timestamp/", TimestampShow).Methods("GET")
-	router.HandleFunc("/api/timestamp/{datestring}", TimestampShow).Methods("GET")
-	
+	router := NewRouter()
+
 	log.Fatal(http.ListenAndServe("localhost:8000", router))
 }
